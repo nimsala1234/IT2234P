0563b59df720f19645770949a97b379a712dbbbb
Dinithi Nimsala <47084360+nimsala1234@users.noreply.github.com>
2025-03-14 21:01:43 +0530

    Add files via upload

---
+++ 2025.3.10/Codes/revnum.js
@@ -0,0 +1,24 @@
+console.log("Reverse number")
+let num=17
+let mid = parseInt(num/2)+1
+let tmid=mid-1
+let start=1
+let end=num
+let tend=end
+
+while(start<end){
+	if(tmid>0){
+		console.log(tmid)
+		tmid--
+	}
+	else if(start == mid){
+		console.log("-"+mid+"-")
+	}
+		
+	else{
+		console.log(tend)
+		tend--
+	}
+	start++
+	
+}
