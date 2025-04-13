b4d552489b007147e806fde90f1af37953853144
Dinithi Nimsala <47084360+nimsala1234@users.noreply.github.com>
2025-04-13 13:14:13 +0530

    Update index.js

--- 2025.4.7/Codes/index.js
+++ 2025.4.7/Codes/index.js
@@ -4,7 +4,7 @@ const port = 3001;
 
 const studentRoute = require('./Student/studentroute')
 
-app.use(express.json)
+app.use(express.json())
 app.use('/student', studentRoute)
 
 app.listen(port,()=>{
